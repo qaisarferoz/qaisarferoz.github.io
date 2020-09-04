e057ad39ba5cf3802aaf19bbeb605fd41ee6e070
Qaisar feroz <49122346+qaisarferoz@users.noreply.github.com>
2020-09-05 01:04:45 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function init() {
   Tabletop.init( {
-    key: 'https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
+    key: 'https://docs.google.com/spreadsheets/d/1wQJQJH5mQgMoR5-Xtt_mUWR_pWwvEfb6XZwK2vyXn-0/edit?usp=sharing',
     simpleSheet: true }
   ).then(function(data, tabletop) { 
     console.log(data)
